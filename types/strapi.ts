f4935cd4a25b9ea7ef0207435191a73703a8733c
Aaliyah <lammaituyen949@gmail.com>
2026-02-04 15:26:26 +0700

    add: strapi image type

--- types/strapi.ts
+++ types/strapi.ts
@@ -24,3 +24,43 @@ export type StrapiSingleResponse<T> = {
   data: StrapiEntity<T> | null;
   meta?: Record<string, unknown>;
 };
+
+export type StrapiImageFormat = {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: string | null;
+  size: number;
+  width: number;
+  height: number;
+  sizeInBytes: number;
+};
+
+export type StrapiImageFormats = {
+  thumbnail?: StrapiImageFormat;
+  small?: StrapiImageFormat;
+  medium?: StrapiImageFormat;
+  large?: StrapiImageFormat;
+};
+
+export type StrapiImageAttributes = {
+  name: string;
+  alternativeText: string | null;
+  caption: string | null;
+  width: number;
+  height: number;
+  formats: StrapiImageFormats | null;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: string | null;
+  provider: string;
+  provider_metadata: Record<string, unknown> | null;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+};
