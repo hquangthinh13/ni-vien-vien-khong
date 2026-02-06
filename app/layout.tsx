61cd1121b3e5ac61c45968009e21074c14a81082
hquangthinh13 <hquangthinh13@gmail.com>
2026-02-06 14:04:02 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,10 +30,7 @@ const firaCode = Fira_Code({
 
 export const metadata: Metadata = {
   icons: {
-    icon: [
-      { url: "/favicon.svg", type: "image/svg+xml" },
-      { url: "/favicon.ico", sizes: "any" },
-    ],
+    icon: [{ url: "/favicon.svg", type: "image/svg+xml" }],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
   },
   title: "Ni Viện Viên Không",
