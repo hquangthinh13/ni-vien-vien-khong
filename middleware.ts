7b89a4592268b4613acddafe0e2ef4baa9446f0e
hquangthinh13 <hquangthinh13@gmail.com>
2026-03-18 20:29:08 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import createMiddleware from "next-intl/middleware";
 
-export default createMiddleware({
-  locales: ["vi", "en"],
-  defaultLocale: "vi",
-});
+// export default createMiddleware({
+//   locales: ["vi", "en"],
+//   defaultLocale: "vi",
+// });
 
 export const config = {
   matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
