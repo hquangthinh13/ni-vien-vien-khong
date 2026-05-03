26151d273c436baa283e4eb63c1fc07d38383710
hquangthinh13 <hquangthinh13@gmail.com>
2026-05-03 22:39:26 +0700

    Update CourseregistrationDialog.tsx

--- src/features/courseRegistration/ui/CourseregistrationDialog.tsx
+++ src/features/courseRegistration/ui/CourseregistrationDialog.tsx
@@ -31,7 +31,7 @@ export default function ActivityRegistrationDialog({
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild disabled={true}>
+      <DialogTrigger asChild disabled={formOpened === false || !active}>
         <button
           className={cn(
             " text-left w-full group/reg relative cursor-pointer overflow-hidden rounded-2xl border border-primary/20 bg-primary/5 p-4 transition-all duration-300 hover:bg-primary/10 hover:shadow-md",
